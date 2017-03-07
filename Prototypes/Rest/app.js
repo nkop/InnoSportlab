01e6c675a93ddf25f6d90926fd526feafa38e5e0
nkop <nkop1@avans.nl>
2017-03-07 13:47:59 +0100

    online db connectie

--- Prototypes/Rest/app.js
+++ Prototypes/Rest/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/SperoVideo');
+mongoose.connect('mongodb://admin:admin@innosportlab-shard-00-00-0tqf6.mongodb.net:27017,innosportlab-shard-00-01-0tqf6.mongodb.net:27017,innosportlab-shard-00-02-0tqf6.mongodb.net:27017/InnoSportlab?ssl=true&replicaSet=InnoSportlab-shard-0&authSource=admin');
 mongoose.Promise = require('q').Promise;
 
 //models
